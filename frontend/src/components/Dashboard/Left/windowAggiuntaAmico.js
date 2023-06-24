a4085d78208c2e805a4cb64886394952bb136fc6
Alessandro Genco <alessandrogenco8@gmail.com>
2023-06-24 15:38:55 +0200

    no message

---
+++ frontend/src/components/Dashboard/Left/windowAggiuntaAmico.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function windowAggiuntaAmico() {
+
+    return(
+        <Container className="position-absolute top-50 start-50">
+            <div>
+                <input type="text"/>
+            </div>
+        </Container>
+    )
+
+}
